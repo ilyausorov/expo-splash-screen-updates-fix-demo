398750f422915076beb23725ec1dee4490bd1024
Ilya Usorov <ilya@usorov.com>
2022-05-20 21:18:26 -0400

    Rdy for testing

--- App.tsx
+++ App.tsx
@@ -1,22 +1,153 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import AppLoading from "expo-app-loading";
+import { Asset } from "expo-asset";
+import Constants from "expo-constants";
+import * as SplashScreen from "expo-splash-screen";
+import * as Updates from "expo-updates";
+import { useCallback, useEffect, useMemo, useState } from "react";
+import {
+  Animated,
+  Button,
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  DevSettings,
+} from "react-native";
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+// Instruct SplashScreen not to hide yet, we want to do this manually
+SplashScreen.preventAutoHideAsync().catch(() => {
+  /* reloading the app might trigger some race conditions, ignore them */
+});
 
 export default function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
+  return (
+    <AnimatedAppLoader image={require('./assets/images/splash.png')}>
+      <MainScreen />
+    </AnimatedAppLoader>
+  );
+}
 
-  if (!isLoadingComplete) {
-    return null;
-  } else {
+function AnimatedAppLoader({ children, image }) {
+  const [isSplashReady, setSplashReady] = useState(false);
+
+  const startAsync = useCallback(
+    // If you use a local image with require(...), use `Asset.fromModule`
+    () => Asset.fromModule(image).downloadAsync(),
+    [image]
+  );
+
+  const onFinish = useCallback(() => setSplashReady(true), []);
+
+  if (!isSplashReady) {
     return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
+      <AppLoading
+        // Instruct SplashScreen not to hide yet, we want to do this manually
+        autoHideSplash={false}
+        startAsync={startAsync}
+        onError={console.error}
+        onFinish={onFinish}
+      />
     );
   }
+
+  return <AnimatedSplashScreen image={image}>{children}</AnimatedSplashScreen>;
+}
+
+function AnimatedSplashScreen({ children, image }) {
+  const animation = useMemo(() => new Animated.Value(1), []);
+  const [isAppReady, setAppReady] = useState(false);
+  const [isSplashAnimationComplete, setAnimationComplete] = useState(false);
+
+  useEffect(() => {
+    if (isAppReady) {
+      Animated.timing(animation, {
+        toValue: 0,
+        duration: 2000,
+        useNativeDriver: true,
+      }).start(() => setAnimationComplete(true));
+    }
+  }, [isAppReady]);
+
+  const onImageLoaded = useCallback(async () => {
+    try {
+      await SplashScreen.hideAsync();
+      // Load stuff
+      await Promise.all([]);
+    } catch (e) {
+      // handle errors
+    } finally {
+      setAppReady(true);
+    }
+  }, []);
+
+  return (
+    <View style={{ flex: 1 }}>
+      {isAppReady && children}
+      {!isSplashAnimationComplete && (
+        <Animated.View
+          pointerEvents="none"
+          style={[
+            StyleSheet.absoluteFill,
+            {
+              backgroundColor: Constants.manifest.splash.backgroundColor,
+              opacity: animation,
+            },
+          ]}
+        >
+          <Animated.Image
+            style={{
+              width: "100%",
+              height: "100%",
+              resizeMode: Constants.manifest.splash.resizeMode || "contain",
+              transform: [
+                {
+                  scale: animation,
+                },
+              ],
+            }}
+            source={image}
+            onLoadEnd={onImageLoaded}
+            fadeDuration={0}
+          />
+        </Animated.View>
+      )}
+    </View>
+  );
+}
+
+function MainScreen() {
+  const onReloadPress = useCallback(() => {
+    if (Platform.OS === "web") {
+      location.reload();
+    } else {
+      if (__DEV__) {
+        return DevSettings.reload();
+      } else {
+        return Updates.reloadAsync();
+      }
+    }
+  }, []);
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "plum",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Text
+        style={{
+          color: "black",
+          fontSize: 30,
+          marginBottom: 15,
+          fontWeight: "bold",
+        }}
+      >
+        Pretty Cool!
+      </Text>
+      <Button title="Run Again" onPress={onReloadPress} />
+    </View>
+  );
 }
